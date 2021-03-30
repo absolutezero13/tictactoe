385b2de795c67fa08c196490de6f206e10b3cd3b
unknown <ilker897@gmail.com>
2021-03-30 23:07:21 +0300

    5 round implemented

--- src/Components/Board.tsx
+++ src/Components/Board.tsx
@@ -5,70 +5,78 @@ import Square from "./Square";
 const Board: React.FC = () => {
   const [squares, setSquares] = useState(new Array(9).fill(""));
   const [activePlayer, setActivePlayer] = useState("Player 1");
-  const [winner, setWinner] = useState<any>("");
+  const [roundWinner, setRoundWinner] = useState<any>("");
   const [roundNumber, setRoundNumber] = useState(1);
   const [player1Score, setPlayer1Score] = useState(0);
   const [player2Score, setPlayer2Score] = useState(0);
   const [gameWinner, setGameWinner] = useState("");
+
+  ///////////////////////////////////////////////////////
   useEffect(() => {
-    if (roundNumber > 5 || player2Score >= 3 || player1Score >= 3) {
+    if (player2Score >= 3 || player1Score >= 3) {
       player1Score > player2Score
         ? setGameWinner("Player 1")
         : setGameWinner("Player 2");
-    } else {
-      if (isGameOver(squares) && !winner) {
-        setRoundNumber((prevNum) => prevNum + 1);
-        if (isGameOver(squares) === "X") {
-          setPlayer1Score((prevScore) => prevScore + 1);
-        }
-        if (isGameOver(squares) === "O") {
-          setPlayer2Score((prevScore) => prevScore + 1);
-        }
-        setWinner(isGameOver(squares));
-        setTimeout(() => {
-          reset();
-        }, 2500);
+    }
+  }, [roundWinner]);
+
+  useEffect(() => {
+    if (roundWinner && !gameWinner && player2Score < 3 && player1Score < 3) {
+      setTimeout(() => {
+        reset();
+      }, 2000);
+    }
+  }, [roundWinner]);
+
+  useEffect(() => {
+    const xOrO = isGameOver(squares);
+    if (xOrO && !roundWinner && !gameWinner) {
+      setRoundNumber((prevNum) => prevNum + 1);
+      if (xOrO === "X") {
+        setPlayer1Score((prevScore) => prevScore + 1);
       }
+      if (xOrO === "O") {
+        setPlayer2Score((prevScore) => prevScore + 1);
+      }
+      setRoundWinner(xOrO);
     }
   }, [squares]);
 
   const clickSquare = (id: number): void => {
-    if (!squares[id] && !winner) {
+    if (!squares[id] && !roundWinner && !gameWinner) {
       if (activePlayer === "Player 1") {
         setActivePlayer("Player 2");
       } else {
         setActivePlayer("Player 1");
       }
     }
-
     setSquares((prevSquares) => {
       return prevSquares.map((square, i) => {
-        if (i === id && !square && !winner) {
+        if (i === id && !square && !roundWinner && !gameWinner) {
           square = activePlayer === "Player 1" ? "X" : "O";
         }
-
         return square;
       });
     });
   };
 
   const reset = () => {
-    setWinner("");
+    setRoundWinner("");
     setSquares(new Array(9).fill(""));
     setActivePlayer("Player 1");
   };
 
   return (
     <div className="board">
-      <h2>Round {roundNumber}</h2>
-      <div style={{ display: "flex" }}>
+      {!roundWinner && <h2>Round {roundNumber}</h2>}
+      <div className="board__score">
         <h1>Player 1 </h1>{" "}
         <h2 style={{ color: "red" }}>
           {player1Score} : {player2Score}
         </h2>
         <h1>Player 2</h1>
       </div>
-      <div className="game-field">
+      <div className="board__game-field">
         {squares.map((square, i) => {
           return (
             <Square clickSquare={clickSquare} square={square} key={i} i={i} />
@@ -80,13 +88,13 @@ const Board: React.FC = () => {
           marginRight: activePlayer === "Player 1" ? "250px" : "-250px",
         }}
       >
-        {!winner && activePlayer + "'s turn"}
+        {!roundWinner && activePlayer + "'s turn"}
       </p>
       <p>
-        {winner &&
-          (winner === "X"
+        {roundWinner &&
+          (roundWinner === "X"
             ? "Player 1 Wins the Round"
-            : winner === "O"
+            : roundWinner === "O"
             ? "Player 2 Wins the Round"
             : null)}
       </p>
